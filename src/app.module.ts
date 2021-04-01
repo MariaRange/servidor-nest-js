ee86c2692803a0db2a0983317b853cc059a9d427
maria rangel <maria.rangel.is@unipolidgo.edu.mx>
2021-03-31 19:18:11 -0600

    ImagenesServidasEstaticamente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports:[
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'avatars')
+    }),
     TypeOrmModule.forRoot(),
     UsuariosModule,
     CategoriesModule
